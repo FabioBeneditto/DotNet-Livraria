8d49638b3e705c55a6eb89294885f84c66997223
Fabio Beneditto <fabiobeneditto@gmail.com>
2015-03-27 13:27:50 -0300

    Initial Commit
    Repositório da aula de C# do dia 20/03/2015, na Universidade Feevale

---
+++ Livraria/Autor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Livraria
+{
+    public class Autor
+    {
+
+        #region Variaveis Privadas
+
+        #endregion
+
+        #region Propriedades Publicas
+
+        #endregion
+
+        public Autor()
+        {
+            
+        }
+    }
+}
---
+++ Livraria/Livro.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Livraria
+{
+    public class Livro
+    {
+        #region Variaveis Privadas
+        private int codLivro;
+        private string titulo;
+        private int numPaginas;
+        private int ano;
+        private string editora;
+        private decimal preco;
+        private Autor autor;
+        #endregion
+
+        #region Metodos Publicos
+        public int CodLivro
+        {
+            get { return codLivro; }
+            set { codLivro = value; }
+        }
+
+        public string Titulo
+        {
+            get { return titulo; }
+            set { titulo = value; }
+        }
+
+        public int NumPaginas
+        {
+            get { return numPaginas; }
+            set { numPaginas = value; }
+        }
+
+        public int Ano
+        {
+            get { return ano; }
+            set { ano = value; }
+        }
+
+        public string Editora
+        {
+            get { return editora; }
+            set { editora = value; }
+        }
+
+        public decimal Preco
+        {
+            get { return preco; }
+            set { preco = value; }
+        }
+
+        public Autor Autor
+        {
+            get { return autor; }
+            set { autor = value; }
+        }
+        #endregion
+
+        public Livro()
+        {
+
+        }
+    }
+}
